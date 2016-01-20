dc5c32a95c2cfbe420d1814d859dc3f3b8eb8bcd
Oleg2323 <shyks2323@gmail.com>
2016-01-20 12:20:13 +0200

    Second activity created

--- app/src/main/java/ua/com/shyker/remindme/SecondActivity.java
+++ app/src/main/java/ua/com/shyker/remindme/SecondActivity.java
@@ -1,14 +1,15 @@
 package ua.com.shyker.remindme;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
-
+/**
+ * Created by Олег on 20.01.2016.
+ */
+public class SecondActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_second);
     }
 }
