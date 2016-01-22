ff444bc98d306a1006b7bddf10f50c3136a29d55
Oleg2323 <shyks2323@gmail.com>
2016-01-22 21:51:55 +0200

    Added Toolbar and search icon

---
+++ app/src/main/java/ua/com/shyker/remindme/Constants.java
@@ -0,0 +1,10 @@
+package ua.com.shyker.remindme;
+
+/**
+ * Created by Олег on 22.01.2016.
+ */
+public class Constants {
+    public static final int TAB_ONE = 0;
+    public static final int TAB_TWO = 1;
+    public static final int TAB_THREE = 2;
+}
---
+++ app/src/main/java/ua/com/shyker/remindme/fragment/ExampleFragment.java
@@ -0,0 +1,28 @@
+package ua.com.shyker.remindme.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import ua.com.shyker.remindme.R;
+
+public class ExampleFragment extends Fragment {
+    private View view;
+
+    public static ExampleFragment getInstance(){
+        Bundle args = new Bundle();
+        ExampleFragment fragment = new ExampleFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        view =  inflater.inflate(R.layout.fragment_example, container, false);
+        return view;
+    }
+}
